439f2c869752fcd5b40668da61f45af18ed3a419
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-08-06 11:43:43 -0500

    Update copyright header and thirdparty

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -1,17 +1,12 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+/*
+ * This file includes code derived from HiGHS.jl, originally available at:
+ * https://github.com/ERGO-Code/HiGHS.jl
+ *
+ * Copyright (c) Mathieu Besançon, Oscar Dowson, and contributors
+ * Licensed under the MIT License. See LICENSE_MIT_HIGHS.txt for details.
+ *
+ * Modifications by Ramakrishna Prabhu, Rajesh Gandham from NVIDIA 2025.
+ */
 
 import MathOptInterface as MOI
 const CleverDicts = MOI.Utilities.CleverDicts
